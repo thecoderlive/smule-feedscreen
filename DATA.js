3e92784f84ff8db7af0ef1224aff8f09f3a38c1b
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-12-07 14:15:42 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,45 @@
+export const item = {
+    "id": "1380ee43-8d6b-4ed2-9136-0afec9c2173d",
+    "home_feed": [
+        {
+            "id": "8d88b8fd-eacb-4375-93c5-ef4dbeb22c0a",
+            "user_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/158.png",
+            "user_name": "main",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/35.png",
+            "song_name": "religious",
+            "hashtag": "fine"
+        },
+        {
+            "id": "38a8a13f-7726-46c5-8d69-35880b28c258",
+            "user_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/281.png",
+            "user_name": "effort",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/123.png",
+            "song_name": "debate",
+            "hashtag": "together"
+        },
+        {
+            "id": "d57f260f-bb94-4998-96bb-918161317f1c",
+            "user_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/22.png",
+            "user_name": "avoid",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/200.png",
+            "song_name": "ago",
+            "hashtag": "condition"
+        },
+        {
+            "id": "12aa61e0-2642-4511-9df3-1099b9797f5c",
+            "user_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/295.png",
+            "user_name": "begin",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/365.png",
+            "song_name": "carry",
+            "hashtag": "there"
+        },
+        {
+            "id": "fc4abc6f-5f4d-4f19-8976-0bbe653870f5",
+            "user_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/139.png",
+            "user_name": "read",
+            "song_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/172.png",
+            "song_name": "because",
+            "hashtag": "fire"
+        }
+    ]
+}
